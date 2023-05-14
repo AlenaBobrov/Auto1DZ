b4e68cc018d278adb8c3e8f06a98f4930bbc9385
Alena Bobrova <kaisa8721@gmail.com>
2023-05-14 14:04:32 +0300

    Third commit

---
+++ src/test/java/ru/netology/TestCashbackHackService.java
@@ -0,0 +1,51 @@
+package ru.netology;
+
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class TestCashbackHackService {
+     @Test
+     public void shouldBuyPositive(){
+         CashbackHackService service = new CashbackHackService();
+         int amount = 500;
+         int expected = 500;
+         int actual = service.remain(amount);
+         assertEquals(actual, expected);
+     }
+    @Test
+    public void shouldBuyLimit(){
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int expected = 0;
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void shouldBuyMin(){
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1;
+        int expected = 999;
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void shouldBuyMax(){
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+        int expected = 1;
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void shouldBuyZero(){
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+        int expected = 1000;
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+    }
+
+
+
+}
